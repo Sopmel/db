81c19565cd4819a648dba035badbe5757ba3a646
Sopmel <soph.melin@gmail.com>
2024-02-21 09:42:51 +0100

    create database test

--- create-database.js
+++ create-database.js
@@ -1,8 +1,32 @@
 import mongoose, { connect } from "mongoose";
 
-const con = await connect("mongodb://127.0.0.1:27017/gouptask");
+const con = await connect("mongodb://127.0.0.1:27017/grouptask");
 
 const { db } = mongoose.connection;
+
+const SuppliersSchema = new mongoose.Schema({
+    Name: {type: String},
+    Contact: {type: String}  
+});
+
+const supplierModel = mongoose.model("Suppliers", SuppliersSchema);
+
+
+
+let suppliersCollection = await supplierModel.createCollection();
+
+suppliersCollection.insertMany(
+    [     
+       {
+        Name: "Electronics Supplier Inc",
+        Contact: "John Doe (john@electronicsupplier.com)"
+       },
+       {
+        Name: "Fashion Supplier Co",
+        Contact: "Jane Smith (jane@fashionsupplier.com)"
+       }
+    ]
+);
 
 const productsSchema = new mongoose.Schema({
     Name: { type: String },
@@ -10,6 +34,7 @@ const productsSchema = new mongoose.Schema({
     Price: { type: Number },
     Cost: { type: Number },
     Stock: { type: Number },
+    SupplierId: {type: String}
    
 });
 
@@ -19,4 +44,32 @@ const productModel = mongoose.model("Products", productsSchema);
 
 let productsCollection = await productModel.createCollection();
 
-productsCollection.insertMany()
+productsCollection.insertMany(
+    [
+       { 
+        Name: "Laptop",
+        Category: "Electronics",
+        Price: 1000,
+        Cost: 800,
+        Stock: 50,
+        SupplierId: "65d5b5b413a069370e593b7c"
+       },
+       {
+        Name: "Smartphone",
+        Category: "Electronics",
+        Price: 800,
+        Cost: 600,
+        Stock: 40,
+        SupplierId: "65d5b5b413a069370e593b7c"
+       },
+       {
+        Name: "T-shirt",
+        Category: "Clothing",
+        Price: 20,
+        Cost: 10,
+        Stock: 100,
+        SupplierId: "65d5b5b413a069370e593b7d"
+       }
+    ]
+)
+
