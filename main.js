d765ebc3780b9fffb98d620c2657b87fd0dd363a
AlexanderLindgren1 <alexprestoxxx@gmail.com>
2024-02-22 13:46:42 +0100

    test commit

--- main.js
+++ main.js
@@ -42,6 +42,7 @@ console.log("12. View suppliers")
 console.log("13. View all sales")
 console.log("14. View sum of all profits")
 console.log("15. Close app")
+console.log("test");
 
 let runApp = true;
 
